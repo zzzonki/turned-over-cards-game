ea0685a436f5daaafef84f2284d5d3b8607ce6f9
zzzonki <zzzonki@gmail.com>
2020-08-04 23:46:53 +0300

    timer goes

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -3,16 +3,35 @@ import'./style.css'
 
 export default class Timer extends Component{
     state = {
-        gameStart: false
+        gameStart: false,
+        mins: 0,
+        secs: 0
+    }
+    componentDidMount(){
+        this.timerGo()
     }
     render(){
-        const mins = 0
-        const secs = 0
+        const minutes = this.state.mins < 10 ? "0" + this.state.mins : this.state.mins
+        const seconds = this.state.secs < 10 ? "0" + this.state.secs : this.state.secs
         return(
             <div>
                 <h3>Timer</h3>
-                <div className='timer-field'>{mins} : {secs}</div>
+                <div className='timer-field'>{minutes} : {seconds}</div>
             </div>
         )
+    }
+    timerGo = () =>{
+        setInterval(() => {
+            if (this.state.secs >= 59) {
+                this.setState({
+                    secs: 0,
+                    mins: this.state.mins + 1
+                })
+            } else{
+                this.setState({ 
+                    secs: this.state.secs + 1
+                })
+            }
+        }, 1000)
     }
 }
