d1f8a5a0bc7574e44f3afe969f4d1ad94cef0056
zzzonki <zzzonki@gmail.com>
2020-09-24 17:48:59 +0300

    Updates

---
+++ precache-manifest.a53f4cea4f3f0ef6c224d58f56dedb16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6e9341fc5cbd67c2add3a208e441cf8",
+    "url": "/turned-over-cards-game/index.html"
+  },
+  {
+    "revision": "d6bff69f60469222c5b1",
+    "url": "/turned-over-cards-game/static/css/main.7cfed3a8.chunk.css"
+  },
+  {
+    "revision": "24a5d4b41d5b2a4be028",
+    "url": "/turned-over-cards-game/static/js/2.65457f84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turned-over-cards-game/static/js/2.65457f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6bff69f60469222c5b1",
+    "url": "/turned-over-cards-game/static/js/main.29e50371.chunk.js"
+  },
+  {
+    "revision": "e1f0a7b147c0e9f8deb5",
+    "url": "/turned-over-cards-game/static/js/runtime-main.c4e8603b.js"
+  }
+]);
