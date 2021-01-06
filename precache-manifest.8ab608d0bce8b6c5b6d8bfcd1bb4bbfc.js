db193ee876708d4b9d8f8900c5f9902c2503e46a
Кирилл Иванченко <ivanchenkokv91@gmail.com>
2021-01-06 14:44:33 +0300

    Updates

---
+++ precache-manifest.8ab608d0bce8b6c5b6d8bfcd1bb4bbfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c5e45244cfdf7d04a41ae9755bbe8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db05bdec9866901e5ac4",
+    "url": "/static/css/main.0b76b8ee.chunk.css"
+  },
+  {
+    "revision": "24a5d4b41d5b2a4be028",
+    "url": "/static/js/2.65457f84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65457f84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db05bdec9866901e5ac4",
+    "url": "/static/js/main.87a6559c.chunk.js"
+  },
+  {
+    "revision": "f04100cd5d16444fc2e9",
+    "url": "/static/js/runtime-main.59bf84fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/turned-over-cards-game/precache-manifest.b510f8eade734c65b7ed985efb72eebf.js"
+  "/precache-manifest.8ab608d0bce8b6c5b6d8bfcd1bb4bbfc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/turned-over-cards-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
